df1f261ffb56b34e9c5f7cec4a022440ce001f28
Anand jha <59723738+OCTO20122020-Anand@users.noreply.github.com>
2021-05-28 10:20:13 +0530

    Update clientapp.py

--- clientapp.py
+++ clientapp.py
@@ -35,7 +35,7 @@ def predictRoute():
     return jsonify(result)
 
 
-# port = int(os.getenv("PORT"))
+port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     clApp = ClientApp()
     # app.run(host='0.0.0.0', port=port)
